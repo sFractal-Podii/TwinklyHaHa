a0ed1969b17b561848cd50fe090b7ac2609d96b4
WanjikuMac <edwardscaroline32@gmail.com>
2022-08-25 11:55:51 +0300

    remove :gettext in compiler as it's no longer required

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Twinklyhaha.MixProject do
       version: "0.4.3",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
