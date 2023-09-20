8670a2ab4a7f138ae72e8c0dae8ea81db5f5ef97
WanjikuMac <edwardscaroline32@gmail.com>
2023-09-20 08:25:42 +0300

    fix failing liveview elements due to upgrade

--- lib/twinklyhaha_web/views/page_view.ex
+++ lib/twinklyhaha_web/views/page_view.ex
@@ -1,7 +1,8 @@
 defmodule TwinklyhahaWeb.PageView do
   use TwinklyhahaWeb, :view
+  import Phoenix.Component
 
-  def render("sbom.html", _assigns) do
+  def render("sbom.html", assigns) do
     files =
       :twinklyhaha
       |> Application.app_dir("/priv/static/.well-known/sbom")
@@ -12,11 +13,11 @@ defmodule TwinklyhahaWeb.PageView do
         Map.put(acc, filter, filter_files(files, filter))
       end)
 
-    ~E"""
+    ~H"""
     <p>SBOMs for this site are available in several formats and serializations. </p>
-    <%= for {k, v} <- sbom_files do %>
-      <ol> <%= k %> </ol>
-      <%= for file <- v do %>
+    <%= for {bom_name, bom_file} <- sbom_files do %>
+      <ol> <%= bom_name %> </ol>
+      <%= for file <- bom_file do %>
           <li> <%= link file,  to: ["sbom/",file] %> </li>
       <% end %>
     <% end %>
