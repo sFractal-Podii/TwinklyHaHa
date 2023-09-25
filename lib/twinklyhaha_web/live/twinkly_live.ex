d2c0a653f6ed009fe1a1c4aa4630c2a87dcabf59
sigu <sigumagwa@gmail.com>
2023-09-25 16:03:34 +0300

    upgrade elixir version

--- lib/twinklyhaha_web/live/twinkly_live.ex
+++ lib/twinklyhaha_web/live/twinkly_live.ex
@@ -1,8 +1,9 @@
 defmodule TwinklyhahaWeb.TwinklyLive do
   @moduledoc "Live view for the LED"
 
-  require Logger
   use TwinklyhahaWeb, :live_view
+
+  require Logger
 
   @colors ["Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"]
 
@@ -17,16 +18,17 @@ defmodule TwinklyhahaWeb.TwinklyLive do
 
   @impl true
   def render(assigns) do
-    ~L"""
+    ~H"""
     <div class="row">
     <div class="column column-50 column-offset-25">
         <%= for row <- 0..7 do %>
           <%= for column <- 0..7 do %>
             <div class="led-box">
-            <div class="led led-<%= if @led_on?, do: "on", else: "off" %>" id="led-<%=row %><%= column%>" <%= assign_color(assigns, @current_color, row) %> phx-hook="LedColor"></div>
+            <div class={["led", "led-#{if @led_on?, do: "on", else: "off"}"]} id={"led-#{row}#{column}"} 
+                data-ledcolor={if @current_color == "rainbow", do: Stream.cycle(@colors) |> Enum.at(row), else: @current_color} phx-hook="LedColor"></div>
             </div>
           <% end %>
-            <br/ >
+           <br />
         <% end %>
         <%= if @led_on?, do: select_color(assigns) %>
         <div>
@@ -37,31 +39,16 @@ defmodule TwinklyhahaWeb.TwinklyLive do
     """
   end
 
-  defp assign_color(assigns, "rainbow", row) do
-    ~L"""
-    data-ledcolor="<%= Stream.cycle(@colors) |> Enum.at(row) %>"
-    """
-  end
-
-  defp assign_color(assigns, color, _row) do
-    ~L"""
-    data-ledcolor="<%= color %>"
-    """
-  end
-
   defp select_color(assigns) do
-    # this is assigned here to stop it from updating the select list when colors is shifted to the right
-    colors = @colors
-
-    ~L"""
+    ~H"""
     <form phx-change="change-color">
       <select id="select-colors" name="color">
-      <%= for color <- colors do %>
-          <option value="<%= color %>" <%= if @current_color == color, do: "selected" %> >
+      <%= for color <- @colors do %>
+          <option value={color} selected={if @current_color == color, do: "selected"}  >
             <%= color %>
           </option>
         <% end %>
-      <option value="rainbow" <%= if @current_color == "rainbow", do: "selected" %> >
+      <option value={"rainbow"} selected={if @current_color == "rainbow", do: "selected" }>
         Rainbow
       </option>
     </select>
@@ -71,9 +58,9 @@ defmodule TwinklyhahaWeb.TwinklyLive do
 
   @impl true
   def handle_event("toggle-led", _, socket) do
-    socket = toggle_led(socket)
+    socket = assign(socket, :led_on?, !socket.assigns.led_on?)
     color = hd(socket.assigns.colors)
-    {:noreply, socket |> current_color(color, socket.assigns.led_on?)}
+    {:noreply, current_color(socket, color, socket.assigns.led_on?)}
   end
 
   @impl true
@@ -83,8 +70,14 @@ defmodule TwinklyhahaWeb.TwinklyLive do
 
   @impl true
   def handle_info(:shift_color, socket) do
-    Process.send_after(self(), :shift_color, 100)
-    {:noreply, shift_colors(socket)}
+    case socket.assigns.current_color do
+      "rainbow" ->
+        Process.send_after(self(), :shift_color, 100)
+        {:noreply, shift_colors(socket)}
+
+      _ ->
+        {:noreply, socket}
+    end
   end
 
   def handle_info("on", socket) do
@@ -98,8 +91,8 @@ defmodule TwinklyhahaWeb.TwinklyLive do
   end
 
   def handle_info(color, socket)
-      when color == "rainbow" or color == "red" or color == "orange" or color == "yellow" or
-             color == "green" or color == "blue" or color == "indigo" or color == "violet" do
+      when color == "rainbow" or color == "red" or color == "orange" or color == "yellow" or color == "green" or
+             color == "blue" or color == "indigo" or color == "violet" do
     Logger.debug("tlive:hand.info - #{color}")
     {:noreply, current_color(socket, color, true)}
   end
@@ -119,15 +112,11 @@ defmodule TwinklyhahaWeb.TwinklyLive do
     assign(socket, :current_color, "rainbow")
   end
 
-  defp current_color(socket, _color, _led_on? = false) do
+  defp current_color(socket, _color, false = _led_on?) do
     assign(socket, :current_color, "rgba(0, 0, 0, 0.2)")
   end
 
-  defp current_color(socket, color, _led_on? = true) do
+  defp current_color(socket, color, true = _led_on?) do
     assign(socket, :current_color, color)
-  end
-
-  defp toggle_led(socket) do
-    assign(socket, :led_on?, !socket.assigns.led_on?)
   end
 end
