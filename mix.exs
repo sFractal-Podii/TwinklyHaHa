13023aa34c6d9d0036ce074e326dc3bf18876d99
WanjikuMac <edwardscaroline32@gmail.com>
2023-10-24 12:15:09 +0300

    increase develop version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Twinklyhaha.MixProject do
   def project do
     [
       app: :twinklyhaha,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.15.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
