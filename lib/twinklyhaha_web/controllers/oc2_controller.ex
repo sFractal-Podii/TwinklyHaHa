2eaa96cf25fb986a884ea47dc6653c1aa161315d
WanjikuMac <edwardscaroline32@gmail.com>
2023-10-24 11:25:02 +0300

    apply formatting to fix failing CI

--- lib/twinklyhaha_web/controllers/oc2_controller.ex
+++ lib/twinklyhaha_web/controllers/oc2_controller.ex
@@ -6,18 +6,18 @@ defmodule TwinklyhahaWeb.OC2Controller do
 
   def command(conn, params) do
     params
-      |> Jason.encode!()
-      |> Openc2.Oc2.Command.new()
-      |> Openc2.Oc2.Command.do_cmd()
-      |> handle_response(conn)
-   end
+    |> Jason.encode!()
+    |> Openc2.Oc2.Command.new()
+    |> Openc2.Oc2.Command.do_cmd()
+    |> handle_response(conn)
+  end
 
-    defp handle_response(%Openc2.Oc2.Command{target: nil}, conn) do
-      send_resp(conn, :unprocessable_entity, "Oops! no target?")
-    end
+  defp handle_response(%Openc2.Oc2.Command{target: nil}, conn) do
+    send_resp(conn, :unprocessable_entity, "Oops! no target?")
+  end
 
-    defp handle_response(%Openc2.Oc2.Command{target: command}, conn) do
-      Phoenix.PubSub.broadcast(Twinklyhaha.PubSub, @topic, command)
-      json(conn, %{status: :ok})
-    end
+  defp handle_response(%Openc2.Oc2.Command{target: command}, conn) do
+    Phoenix.PubSub.broadcast(Twinklyhaha.PubSub, @topic, command)
+    json(conn, %{status: :ok})
+  end
 end
