e5e7cdbb38bed4046f6d456d751b37816d59b6f7
WanjikuMac <edwardscaroline32@gmail.com>
2023-10-31 10:50:01 +0300

    use command instead of full path

--- lib/twinklyhaha_web/controllers/oc2_controller.ex
+++ lib/twinklyhaha_web/controllers/oc2_controller.ex
@@ -8,16 +8,16 @@ defmodule TwinklyhahaWeb.OC2Controller do
   def command(conn, params) do
     params
     |> Jason.encode!()
-    |> Openc2.Oc2.Command.new()
-    |> Openc2.Oc2.Command.do_cmd()
+    |> Command.new()
+    |> Command.do_cmd()
     |> handle_response(conn)
   end
 
-  defp handle_response(%Openc2.Oc2.Command{target: nil}, conn) do
+  defp handle_response(%Command{target: nil}, conn) do
     send_resp(conn, :unprocessable_entity, "Oops! no target?")
   end
 
-  defp handle_response(%Openc2.Oc2.Command{target: command}, conn) do
+  defp handle_response(%Command{target: command}, conn) do
     Phoenix.PubSub.broadcast(Twinklyhaha.PubSub, @topic, command)
     json(conn, %{status: :ok})
   end
